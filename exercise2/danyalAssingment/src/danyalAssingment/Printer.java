63ccb53580513920b4cc23476a654df71a087704
Danyal <andy10113@outlook.com>
2017-05-20 00:57:02 +0600

    Initial
    Initial

--- exercise2/danyalAssingment/src/danyalAssingment/Printer.java
+++
@@ -1,8 +0,0 @@
-package andyAssingment;
-
-public class Printer {// Printer class which will be used for printing 
-	  public void display() {
-		    System.out.println("I am Printer Class");
-		  }
-
-}
