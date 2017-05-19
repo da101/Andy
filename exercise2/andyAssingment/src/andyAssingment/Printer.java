0a10ac5d20d04185db3bcd5a6a5e18fb460a1d1c
Andy <andy10113@outlook.com>
2017-05-20 00:09:33 +0600

    Assingment
    Final Push with full project

---
+++ exercise2/andyAssingment/src/andyAssingment/Printer.java
@@ -0,0 +1,8 @@
+package andyAssingment;
+
+public class Printer {// Printer class which will be used for printing 
+	  public void display() {
+		    System.out.println("I am Printer Class");
+		  }
+
+}
---
+++ exercise2/andyAssingment/src/andyAssingment/Ticket.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package andyAssingment;
+
+/**
+ * @author Andy
+ *
+ */
+public interface Ticket {// Interface class which models abstraction
+
+	public void setMoiveDay(String weekday);
+	
+	public String getMoiveDay();
+	
+	public void setPersonalTicketType(String ticketType);
+	
+	public String getPersonalTicketType(String ticketType);
+}
